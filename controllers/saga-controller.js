22875509e17fff43820850b474955a7daf3c2416
Paul Gelot <pgelot@gmail.com>
2017-10-31 10:47:22 -0400

    setting up CRUD for the reviews section

--- controllers/saga-controller.js
+++ controllers/saga-controller.js
@@ -1,4 +1,4 @@
-const Review = require('../models/reviews');
+const Review = require('../models/Reviews');
 
 const reviewsController = {};
 
--- routes/saga-routes.js
+++ routes/saga-routes.js
@@ -1,18 +1,19 @@
 const express = require('express');
 
-const sagaRoutes = express.Router()
-const sagaData = require('../saga.json')
-const sagaController = require('../controllers/saga-controller')
+const sagaRouter = express.Router();
+const sagaData = require('../saga.json');
+const sagaController = require('../controllers/saga-controller');
 
 // const sagaData = require('./saga.json');
 
-let comicInfo = sagaData.comics;
-let summary = comicInfo.summary;
-let volumeCovers = comicInfo.cover;
+// let comicInfo = sagaData.comics;
+// let summary = comicInfo.summary;
+// let volumeCovers = comicInfo.cover;
 
 
 
-// sagaRoutes.get('/' ,sagaController.index)
+sagaRouter.get('/' ,sagaController.index);
+sagaRouter.get('/:id', sagaController.show);
 // sagaRoutes.post('/',sagaController.create)
 
 
@@ -28,7 +29,7 @@ let volumeCovers = comicInfo.cover;
 // }
 
 
-module.exports = sagaRoutes
+module.exports = sagaRouter;
 
 
 // http://www.comicvine.com/api/search/?api_key=your-key&query=your-character&format=json
